f70a384ce70fc21c94a449a6ade54134dbbde5fe
DarkLions <darklions888@gmail.com>
2020-05-23 00:07:25 +0200

    Update CrytalShardItem.java

--- main/java/ch/darklions888/SpellStorm/objects/items/CrytalShardItem.java
+++ main/java/ch/darklions888/SpellStorm/objects/items/CrytalShardItem.java
@@ -2,17 +2,20 @@ package ch.darklions888.SpellStorm.objects.items;
 
 import javax.annotation.Nullable;
 
+import ch.darklions888.SpellStorm.enums.MagicSource;
+import ch.darklions888.SpellStorm.enums.ManaPower;
 import net.minecraft.util.text.TextFormatting;
 
 public class CrytalShardItem extends BaseItem
 {
 
-	public CrytalShardItem(int mana,
-			@Nullable String displayName,
+	public CrytalShardItem(
+			MagicSource source,
+			ManaPower mana,
 			@Nullable TextFormatting format,
 			@Nullable boolean hasEffect,
 			Properties properties) 
 	{
-		super(mana, displayName, format, hasEffect, properties);
+		super(source, mana, format, hasEffect, properties);
 	}
 }
