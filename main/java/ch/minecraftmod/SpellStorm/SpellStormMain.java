164cf2be1872158d2da1c095ec6e74c7d393476c
DarkLions <darklions888@gmail.com>
2020-05-22 13:11:44 +0200

    Main class setup is done

---
+++ main/java/ch/minecraftmod/SpellStorm/SpellStormMain.java
@@ -0,0 +1,59 @@
+package ch.minecraftmod.SpellStorm;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(SpellStormMain.MODID)
+@Mod.EventBusSubscriber(modid = SpellStormMain.MODID, bus = Bus.MOD)
+public class SpellStormMain 
+{
+	public static final String MODID = "spellstorm";
+	public static final Logger LOGGER  = LogManager.getLogger();
+	public static SpellStormMain INSTANCE;
+	
+	public SpellStormMain()
+	{
+		final IEventBus Bus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		Bus.addListener(this::CommonSetup);
+		Bus.addListener(this::ClientSetup);
+		
+		SpellStormMain.INSTANCE = this;
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@SubscribeEvent
+	public void CommonSetup(FMLCommonSetupEvent event)
+	{
+		
+	}
+	
+	@SubscribeEvent
+	public void ClientSetup(FMLClientSetupEvent event)
+	{
+		
+	}
+	
+	@SubscribeEvent
+	public void LoadCompleteEvent(FMLLoadCompleteEvent event)
+	{
+		
+	}
+	
+	public static ResourceLocation location(String key)
+	{
+		return new ResourceLocation(SpellStormMain.MODID, key);
+	}
+}
