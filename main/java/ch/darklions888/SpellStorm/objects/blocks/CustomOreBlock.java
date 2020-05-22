e55863c42fa8d7faeeb88db05dc4975c5f8f250d
DarkLions <darklions888@gmail.com>
2020-05-23 00:06:25 +0200

    Customeoreblock class

---
+++ main/java/ch/darklions888/SpellStorm/objects/blocks/CustomOreBlock.java
@@ -0,0 +1,44 @@
+package ch.darklions888.SpellStorm.objects.blocks;
+
+import java.util.Random;
+
+import ch.darklions888.SpellStorm.init.BlockInit;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
+
+public class CustomOreBlock extends Block
+{
+
+	public CustomOreBlock(Properties properties) 
+	{
+		super(properties);
+	}
+	
+	
+	protected int getExperience(Random rand) 
+	{
+		if(this == BlockInit.crystal_ore)
+			return MathHelper.nextInt(rand, 3, 4);
+		else
+			return 0;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void spawnAdditionalDrops(BlockState state, World worldIn, BlockPos pos, ItemStack stack) 
+	{
+		super.spawnAdditionalDrops(state, worldIn, pos, stack);
+	}
+	
+	@Override
+	public int getExpDrop(BlockState state, IWorldReader reader, BlockPos pos, int fortune, int silktouch) 
+	{
+		return silktouch == 0 ? this.getExperience(RANDOM) : fortune > 0 ? this.getExperience(RANDOM) + fortune : 0;
+	}
+	
+
+}
