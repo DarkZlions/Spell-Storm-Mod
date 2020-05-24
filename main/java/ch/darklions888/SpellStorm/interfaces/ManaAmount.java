79fc932133930625b0c0c17d3df86780c55c0f76
DarkLions <darklions888@gmail.com>
2020-05-25 00:39:50 +0200

    Made a universal interface

--- main/java/ch/darklions888/SpellStorm/interfaces/ManaAmount.java
+++
@@ -1,8 +0,0 @@
-package ch.darklions888.SpellStorm.interfaces;
-
-import ch.darklions888.SpellStorm.enums.ManaPower;
-
-public interface ManaAmount 
-{
-	public ManaPower Mana();
-}
--- main/java/ch/darklions888/SpellStorm/interfaces/SourceOrigin.java
+++
@@ -1,8 +0,0 @@
-package ch.darklions888.SpellStorm.interfaces;
-
-import ch.darklions888.SpellStorm.enums.MagicSource;
-
-public interface SourceOrigin 
-{
-	public MagicSource Source();
-}
