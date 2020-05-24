0b4d1dbeea21412d786bb6f0eda2b8e74498aa2e
DarkLions <darklions888@gmail.com>
2020-05-25 00:43:12 +0200

    This class i will move to the main class

---
+++ main/java/ch/darklions888/SpellStorm/util/helpers/ClientEventBusSubscriber.java
@@ -0,0 +1,23 @@
+package ch.darklions888.SpellStorm.util.helpers;
+
+import ch.darklions888.SpellStorm.SpellStormMain;
+import ch.darklions888.SpellStorm.client.gui.ManaInfuserScreen;
+import ch.darklions888.SpellStorm.init.ContainerTypesInit;
+import net.minecraft.client.gui.ScreenManager;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = SpellStormMain.MODID, bus = Bus.MOD, value = Dist.CLIENT)
+public class ClientEventBusSubscriber 
+{
+	@SubscribeEvent
+	public static void clientSetup(FMLClientSetupEvent event)
+	{
+		//ScreenManager.registerFactory(ContainerTypesInit.MANA_INFUSER.get(), ManaInfuserScreen::new);
+		
+		ContainerTypesInit.MANA_INFUSER.ifPresent(container -> ScreenManager.registerFactory(container, ManaInfuserScreen::new));
+	}
+}
