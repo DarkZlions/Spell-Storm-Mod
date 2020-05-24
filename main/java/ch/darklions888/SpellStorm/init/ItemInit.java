324971d434017e3cb19a5da85b50201a5bd4920e
DarkLions <darklions888@gmail.com>
2020-05-25 00:38:57 +0200

    Small changes in Iteminit

--- main/java/ch/darklions888/SpellStorm/init/ItemInit.java
+++ main/java/ch/darklions888/SpellStorm/init/ItemInit.java
@@ -2,6 +2,7 @@ package ch.darklions888.SpellStorm.init;
 
 import ch.darklions888.SpellStorm.SpellStormMain;
 import ch.darklions888.SpellStorm.enums.MagicSource;
+import ch.darklions888.SpellStorm.enums.ManaContainerSize;
 import ch.darklions888.SpellStorm.enums.ManaPower;
 import ch.darklions888.SpellStorm.objects.CustomItemGroup.SpellStormItemGroup;
 import ch.darklions888.SpellStorm.objects.items.BaseBlockItem;
@@ -9,6 +10,8 @@ import ch.darklions888.SpellStorm.objects.items.BaseItem;
 import ch.darklions888.SpellStorm.objects.items.CrytalShardItem;
 import ch.darklions888.SpellStorm.objects.items.PageOfTheWithers;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.Properties;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,17 +19,20 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemInit 
 {
+	private static final ItemGroup TAB = SpellStormItemGroup.INSTANCE;
+	
 	public static final DeferredRegister<Item> REGISTERITEMS = new DeferredRegister<Item>(ForgeRegistries.ITEMS, SpellStormMain.MODID);
 	
 	//item
-	public static final RegistryObject<Item> CRYSTAL = REGISTERITEMS.register("crystal", () -> new CrytalShardItem(MagicSource.NEUTRALMAGIC, ManaPower.LOW, null, true, new Item.Properties().group(SpellStormItemGroup.INSTANCE)));
-	public static final RegistryObject<Item> UNKNOWN_ITEM = REGISTERITEMS.register("unknown", () -> new BaseItem(MagicSource.UNKNOWNMAGIC, ManaPower.VERYHIGH, null, true, new Item.Properties().group(SpellStormItemGroup.INSTANCE)));
+	public static final RegistryObject<Item> CRYSTAL = REGISTERITEMS.register("crystal", () -> new CrytalShardItem(MagicSource.NEUTRALMAGIC, ManaPower.LOW, null, true, new Item.Properties().group(TAB)));
+	public static final RegistryObject<Item> UNKNOWN_ITEM = REGISTERITEMS.register("unknown", () -> new BaseItem(MagicSource.DARKMAGIC, ManaPower.VERYHIGH, null, true, new Item.Properties().group(TAB)));
+	public static final RegistryObject<Item> CRYTSAL_PIECES = REGISTERITEMS.register("crystal_pieces", () -> new BaseItem(MagicSource.NEUTRALMAGIC, ManaPower.VERYLOW, null, true, new Item.Properties().group(TAB)));
 	
 	//Pages
-	public static final RegistryObject<Item> PAGE_OF_THE_WITHER_SKULL = REGISTERITEMS.register("page_of_the_wither_skull", ()  -> new PageOfTheWithers(MagicSource.DARKMAGIC, ManaPower.DARKPOWER, TextFormatting.DARK_RED, true, new Item.Properties().group(SpellStormItemGroup.INSTANCE)));
+	public static final RegistryObject<Item> PAGE_OF_THE_WITHER_SKULL = REGISTERITEMS.register("page_of_the_wither_skull", ()  -> new PageOfTheWithers(ManaContainerSize.OCEAN, MagicSource.DARKMAGIC, ManaPower.DARKPOWER, TextFormatting.DARK_RED, true, new Item.Properties().group(TAB).maxStackSize(1)));
 	
 	//BlockItem
-	public static final RegistryObject<Item> CRYSTAL_ORE_BLOCK = REGISTERITEMS.register("crystal_ore", () -> new BaseBlockItem(MagicSource.NEUTRALMAGIC, ManaPower.MEDIUM, TextFormatting.LIGHT_PURPLE, false, BlockInit.crystal_ore, new Item.Properties().group(SpellStormItemGroup.INSTANCE)));
-	
+	public static final RegistryObject<Item> CRYSTAL_ORE_BLOCK = REGISTERITEMS.register("crystal_ore", () -> new BaseBlockItem(MagicSource.NEUTRALMAGIC, ManaPower.MEDIUM, TextFormatting.LIGHT_PURPLE, false, BlockInit.CRYSTAL_ORE.get(), new Item.Properties().group(TAB)));
+	public static final RegistryObject<Item> MANAINFUSER = REGISTERITEMS.register("manainfuser", () -> new BaseBlockItem(MagicSource.UNKNOWNMAGIC, ManaPower.HIGH, null, false, BlockInit.MANAINFUSER.get(), new Item.Properties().group(TAB)));
 	
 }
