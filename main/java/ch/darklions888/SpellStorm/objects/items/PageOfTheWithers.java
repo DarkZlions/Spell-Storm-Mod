a4fb6277b641570e8aeb1e9802624c6c70e653c5
DarkLions <darklions888@gmail.com>
2020-05-25 00:41:35 +0200

    Update PageOfTheWithers.java

--- main/java/ch/darklions888/SpellStorm/objects/items/PageOfTheWithers.java
+++ main/java/ch/darklions888/SpellStorm/objects/items/PageOfTheWithers.java
@@ -1,45 +1,70 @@
 package ch.darklions888.SpellStorm.objects.items;
 
+import java.util.List;
+
 import ch.darklions888.SpellStorm.enums.MagicSource;
+import ch.darklions888.SpellStorm.enums.ManaContainerSize;
 import ch.darklions888.SpellStorm.enums.ManaPower;
-import net.minecraft.entity.EntityType;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.WitherSkullEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
-public class PageOfTheWithers extends BaseBookItem
+public class PageOfTheWithers extends BasePageItem
 {
-	
-	public PageOfTheWithers(MagicSource source, ManaPower mana, TextFormatting format, boolean hasEffect, Properties properties) 
+	private ItemStack stack = new ItemStack(this);
+	public PageOfTheWithers(ManaContainerSize size, MagicSource source, ManaPower mana, TextFormatting format, boolean hasEffect, Properties properties) 
 	{
-		super(source, mana, format, hasEffect, properties);
+		super(size, source, mana, format, hasEffect, properties);
 	}
 	
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) 
 	{
 		ItemStack stack = playerIn.getHeldItem(handIn);
-		
 		if(worldIn.isRemote)
 		{
+			if(this.getMana(stack) > 0)
+			{
+				worldIn.playSound(playerIn, playerIn.getPosition(), SoundEvents.ENTITY_WITHER_SHOOT, SoundCategory.PLAYERS, 1.0f, 0.1f);
+			}
+			
 			return ActionResult.resultPass(stack);
 		}
 		else
 		{
-			double x = playerIn.getLookVec().x;
-			double y = playerIn.getLookVec().y;
-			double z = playerIn.getLookVec().z;
-			
-			WitherSkullEntity skull = new WitherSkullEntity(worldIn, playerIn.getPosX(), playerIn.getPosY() + 1, playerIn.getPosZ(), x, y, z);
-			
-			worldIn.addEntity(skull);
+			if(this.getMana(stack) > 0)
+			{
+				double speed = 1.2d;
+				
+				double xD = playerIn.getLookVec().getX() * speed;
+				double yD = playerIn.getLookVec().getY() * speed;
+				double zD = playerIn.getLookVec().getZ() * speed;
+				
+				WitherSkullEntity skull = new WitherSkullEntity(worldIn, playerIn.getPosX(), playerIn.getPosY() + 1.5, playerIn.getPosZ(), xD, yD, zD);
+				
+				this.addMana(stack, -1);
+				
+				worldIn.addEntity(skull);
+				
+			}
 			
 			return ActionResult.resultSuccess(stack);
 		}
 	}
+	
+	public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) 
+	{
+		tooltip.add(new StringTextComponent(String.valueOf(this.getMana(stack)) + "/" + this.size + " Mana left"));
+		super.addInformation(stack, worldIn, tooltip, flagIn);
+	}
+
 }
