bbce7dbee8fa8748455570909bd53b781e4ab8e8
DarkLions <darklions888@gmail.com>
2020-05-25 00:38:36 +0200

    Small changes and added Manainfuser

--- main/java/ch/darklions888/SpellStorm/init/BlockInit.java
+++ main/java/ch/darklions888/SpellStorm/init/BlockInit.java
@@ -2,6 +2,7 @@ package ch.darklions888.SpellStorm.init;
 
 import ch.darklions888.SpellStorm.SpellStormMain;
 import ch.darklions888.SpellStorm.objects.blocks.CrystalOreBlock;
+import ch.darklions888.SpellStorm.objects.blocks.ManaInfuserBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.OreBlock;
 import net.minecraft.block.SoundType;
@@ -15,8 +16,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class BlockInit 
 {
 	public static final DeferredRegister<Block> BLOCKSREGISTER = new DeferredRegister<>(ForgeRegistries.BLOCKS, SpellStormMain.MODID);
-	
-	public static final Block crystal_ore = new CrystalOreBlock(Block.Properties.create(Material.ROCK, DyeColor.PURPLE).hardnessAndResistance(3.0f, 6.0f).harvestLevel(3).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE));
-	
-	public static final RegistryObject<Block> CRYSTAL_ORE = BLOCKSREGISTER.register("crystal_ore", () -> crystal_ore);
+
+	public static final RegistryObject<Block> CRYSTAL_ORE = BLOCKSREGISTER.register("crystal_ore", () -> new CrystalOreBlock(Block.Properties.create(Material.ROCK, DyeColor.PURPLE).hardnessAndResistance(3.0f, 6.0f).harvestLevel(3).harvestTool(ToolType.PICKAXE).sound(SoundType.STONE)));
+	public static final RegistryObject<Block> MANAINFUSER = BLOCKSREGISTER.register("manainfuser", () -> new ManaInfuserBlock(Block.Properties.create(Material.IRON, DyeColor.BLACK).hardnessAndResistance(2.5f).harvestLevel(2).harvestTool(ToolType.PICKAXE)));
 }
