07f0ff52ea585b772d392566e13973f040c1313f
DarkLions <darklions888@gmail.com>
2020-05-25 20:30:09 +0200

    Update PageOfTheWithers.java
    The getAbilities is for a future item, that can manage all the pages like a book. To get all the abilites in the pages I added a method to get the ablities.

--- main/java/ch/darklions888/SpellStorm/objects/items/PageOfTheWithers.java
+++ main/java/ch/darklions888/SpellStorm/objects/items/PageOfTheWithers.java
@@ -1,11 +1,8 @@
 package ch.darklions888.SpellStorm.objects.items;
 
-import java.util.List;
-
 import ch.darklions888.SpellStorm.enums.MagicSource;
 import ch.darklions888.SpellStorm.enums.ManaContainerSize;
 import ch.darklions888.SpellStorm.enums.ManaPower;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.WitherSkullEntity;
 import net.minecraft.item.ItemStack;
@@ -13,14 +10,11 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 public class PageOfTheWithers extends BasePageItem
 {
-	private ItemStack stack = new ItemStack(this);
 	public PageOfTheWithers(ManaContainerSize size, MagicSource source, ManaPower mana, TextFormatting format, boolean hasEffect, Properties properties) 
 	{
 		super(size, source, mana, format, hasEffect, properties);
@@ -41,30 +35,29 @@ public class PageOfTheWithers extends BasePageItem
 		}
 		else
 		{
-			if(this.getMana(stack) > 0)
-			{
-				double speed = 1.2d;
-				
-				double xD = playerIn.getLookVec().getX() * speed;
-				double yD = playerIn.getLookVec().getY() * speed;
-				double zD = playerIn.getLookVec().getZ() * speed;
-				
-				WitherSkullEntity skull = new WitherSkullEntity(worldIn, playerIn.getPosX(), playerIn.getPosY() + 1.5, playerIn.getPosZ(), xD, yD, zD);
-				
-				this.addMana(stack, -1);
-				
-				worldIn.addEntity(skull);
-				
-			}
-			
-			return ActionResult.resultSuccess(stack);
+			return this.getAbilities(worldIn, playerIn, handIn, stack);
 		}
 	}
 	
-	public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) 
+	@Override
+	public ActionResult<ItemStack> getAbilities(World worldIn, PlayerEntity playerIn, Hand handIn, ItemStack stack) 
 	{
-		tooltip.add(new StringTextComponent(String.valueOf(this.getMana(stack)) + "/" + this.size + " Mana left"));
-		super.addInformation(stack, worldIn, tooltip, flagIn);
+		if(this.getMana(stack) > 0)
+		{
+			double speed = 1.2d;
+			
+			double xD = playerIn.getLookVec().getX() * speed;
+			double yD = playerIn.getLookVec().getY() * speed;
+			double zD = playerIn.getLookVec().getZ() * speed;
+			
+			WitherSkullEntity skull = new WitherSkullEntity(worldIn, playerIn.getPosX() + xD, playerIn.getPosY() + 1.5, playerIn.getPosZ() + zD, xD, yD, zD);
+			
+			this.addMana(stack, -1);
+			
+			worldIn.addEntity(skull);
+			
+		}
+		
+		return ActionResult.resultSuccess(stack);
 	}
-
 }
