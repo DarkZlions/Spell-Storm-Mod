1d4c9089f27081bfe476731726e051459a3f81f7
DarkLions <darklions888@gmail.com>
2020-05-31 02:07:38 +0200

    Added PageOfFireball

--- main/java/ch/darklions888/SpellStorm/objects/items/PageOfFireballs.java
+++ main/java/ch/darklions888/SpellStorm/objects/items/PageOfFireballs.java
@@ -3,7 +3,16 @@ package ch.darklions888.SpellStorm.objects.items;
 import ch.darklions888.SpellStorm.enums.MagicSource;
 import ch.darklions888.SpellStorm.enums.ManaContainerSize;
 import ch.darklions888.SpellStorm.enums.ManaPower;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.SmallFireballEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 
 public class PageOfFireballs extends BasePageItem
 {
@@ -12,5 +21,65 @@ public class PageOfFireballs extends BasePageItem
 	{
 		super(size, source, mana, format, hasEffect, properties);
 	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) 
+	{
+		return getAbilities(worldIn, playerIn, handIn);
+	}
+	
+	@Override
+	public ActionResult<ItemStack> getAbilities(World worldIn, PlayerEntity playerIn, Hand handIn) 
+	{
+		ItemStack stack = playerIn.getHeldItem(handIn);
+		
+		if(worldIn.isRemote)
+		{
+			if(this.getMana(stack) > 0)
+			{
+				worldIn.playSound(playerIn,playerIn.getPosition(), SoundEvents.ITEM_FIRECHARGE_USE,  SoundCategory.PLAYERS, 1.0f, .1f);
+				for(int i = 0; i < 10; i++)
+				{
+					worldIn.addParticle(ParticleTypes.LAVA, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), playerIn.getLookVec().x + playerIn.getRNG().nextDouble(), playerIn.getLookVec().y + playerIn.getRNG().nextDouble(), playerIn.getLookVec().z + playerIn.getRNG().nextDouble());
+				}
+				
+				return ActionResult.resultSuccess(stack);				
+			}
+			else
+			{
+				return ActionResult.resultFail(stack);
+			}
 
+		}
+		else
+		{	if(this.getMana(stack) > 0)
+			{
+				double accel = 1.5d;	//Bonus acceleration for more momentum or something like this
+				
+				for(int i = 0; i < 3; i++)
+				{
+					SmallFireballEntity entity = new SmallFireballEntity
+							(
+							worldIn,
+							playerIn.getPosX() + playerIn.getLookVec().x,
+							playerIn.getPosY() + 1.2d,
+							playerIn.getPosZ() + playerIn.getLookVec().z,
+							playerIn.getLookVec().x * accel,
+							playerIn.getLookVec().y * accel,
+							playerIn.getLookVec().z * accel
+							);
+					
+					worldIn.addEntity(entity);
+				}
+				
+				this.addMana(stack, -1);
+				
+				return ActionResult.resultSuccess(stack);
+			}
+			else
+			{
+				return ActionResult.resultFail(stack);
+			}
+		}
+	}
 }
