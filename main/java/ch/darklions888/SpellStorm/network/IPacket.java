83a7ebbf96998316841987ba7f608452b3af7a93
DarkLions <darklions888@gmail.com>
2020-08-24 19:57:10 +0200

    Networking is finally working.

---
+++ main/java/ch/darklions888/SpellStorm/network/IPacket.java
@@ -0,0 +1,12 @@
+package ch.darklions888.SpellStorm.network;
+
+import java.util.function.Supplier;
+
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.network.NetworkEvent.Context;
+
+public interface IPacket {
+	void encode(PacketBuffer buf);
+	
+	void handle(Supplier<Context> ctx);
+}
