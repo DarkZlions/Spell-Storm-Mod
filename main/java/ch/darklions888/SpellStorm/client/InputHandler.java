94f75073b648772700c082ce8e5844defb1542bc
DarkLions <darklions888@gmail.com>
2020-08-24 19:55:00 +0200

    InputHandler for Keybinds

--- main/java/ch/darklions888/SpellStorm/client/InputHandler.java
+++ main/java/ch/darklions888/SpellStorm/client/InputHandler.java
@@ -1,5 +1,27 @@
 package ch.darklions888.SpellStorm.client;
 
+import static ch.darklions888.SpellStorm.SpellStormMain.MODID;
+import static org.lwjgl.glfw.GLFW.*;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent.ClientTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
 public class InputHandler {
+	private static final String CATEGORY = "key.categories." + MODID;
 	
+	public static final KeyBinding openBookOfSpellsContainer = new KeyBinding(MODID + ".key.openBookOfSpellsContainer", GLFW_KEY_R, CATEGORY);
+	
+	static {
+		//ClientRegistry.registerKeyBinding(openBookOfSpellsContainer);
+	}
+	
+	@SubscribeEvent
+	public static void OnClientTickEvent(final ClientTickEvent event) {
+		
+	}
 }
