5a8fb96436746377ff531379cc39fd3f52804452
DarkLions <darklions888@gmail.com>
2020-09-11 19:17:26 +0200

    Added ItemColors and Translucentrenderlayer for magical forge.

--- main/java/ch/darklions888/SpellStorm/client/ClientEventBusSubscriber.java
+++ main/java/ch/darklions888/SpellStorm/client/ClientEventBusSubscriber.java
@@ -1,11 +1,14 @@
 package ch.darklions888.SpellStorm.client;
 
+import ch.darklions888.SpellStorm.ItemColorHandler;
 import ch.darklions888.SpellStorm.client.gui.BookOfSpellsScreen;
+import ch.darklions888.SpellStorm.client.gui.MagicalForgeScreen;
 import ch.darklions888.SpellStorm.client.gui.ManaInfuserScreen;
 import ch.darklions888.SpellStorm.client.gui.SoulExtractorScreen;
-import ch.darklions888.SpellStorm.init.BlockInit;
-import ch.darklions888.SpellStorm.init.ContainerTypesInit;
 import ch.darklions888.SpellStorm.lib.Lib;
+import ch.darklions888.SpellStorm.registries.BlockInit;
+import ch.darklions888.SpellStorm.registries.ContainerTypesInit;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
@@ -20,14 +23,15 @@ public class ClientEventBusSubscriber {
 	@SubscribeEvent
 	public static void clientSetup(FMLClientSetupEvent event) {
 
-		ContainerTypesInit.MANA_INFUSER
-				.ifPresent(container -> ScreenManager.registerFactory(container, ManaInfuserScreen::new));
-		ContainerTypesInit.SOUL_EXTRACTOR
-				.ifPresent(container -> ScreenManager.registerFactory(container, SoulExtractorScreen::new));
-		ContainerTypesInit.BOOK_OF_SPELLS
-				.ifPresent(container -> ScreenManager.registerFactory(container, BookOfSpellsScreen::new));
+		ContainerTypesInit.MANA_INFUSER.ifPresent(container -> ScreenManager.registerFactory(container, ManaInfuserScreen::new));
+		ContainerTypesInit.SOUL_EXTRACTOR.ifPresent(container -> ScreenManager.registerFactory(container, SoulExtractorScreen::new));
+		ContainerTypesInit.BOOK_OF_SPELLS.ifPresent(container -> ScreenManager.registerFactory(container, BookOfSpellsScreen::new));
+		ContainerTypesInit.MAGICAL_FORGE.ifPresent(container -> ScreenManager.registerFactory(container, MagicalForgeScreen::new));
 
 		RenderTypeLookup.setRenderLayer(BlockInit.MAGICAL_TREE_SAPLING.get(), RenderType.getCutout());
 		RenderTypeLookup.setRenderLayer(BlockInit.SOUL_EXTRACTOR.get(), RenderType.getTranslucent());
+		RenderTypeLookup.setRenderLayer(BlockInit.MAGICAL_FORGE.get(), RenderType.getTranslucent());
+		
+		ItemColorHandler.init(Minecraft.getInstance().getItemColors());
 	}
 }
