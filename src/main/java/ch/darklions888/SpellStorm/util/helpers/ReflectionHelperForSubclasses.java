10704d0b58c3224d05ad99e21f79b2269d90ca96
DarkLions <darklions888@gmail.com>
2020-09-13 22:04:01 +0200

    Update ReflectionHelperForSubclasses.java

--- src/main/java/ch/darklions888/SpellStorm/util/helpers/ReflectionHelperForSubclasses.java
+++ src/main/java/ch/darklions888/SpellStorm/util/helpers/ReflectionHelperForSubclasses.java
@@ -8,11 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 
-import net.minecraft.entity.ai.attributes.AttributeModifierManager;
-import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper.UnableToAccessFieldException;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper.UnableToFindFieldException;
 
 public class ReflectionHelperForSubclasses extends ObfuscationReflectionHelper {
 	private static final Logger LOGGER = LogManager.getLogger();
