3fa5d7b1aad0f3bbeb90d614b0becafb401b62e7
DarkLions <darklions888@gmail.com>
2020-09-14 22:02:09 +0200

    Removed and Renamed the Types

--- src/main/java/ch/darklions888/SpellStorm/lib/ManaContainerType.java
+++ src/main/java/ch/darklions888/SpellStorm/lib/ManaContainerType.java
@@ -5,11 +5,9 @@ public enum ManaContainerType
 	VERYSMALL(60),
 	SMALL(120),
 	MEDIUM(180),
-	BIG(240),
-	GIANT(300),
-	OCEAN(400),
-	REALLYBIG(800),
-	BIGGER(1600);
+	GIANT(400),
+	GIGANTIC(800),
+	BIG(1600);
 	
 	public int size;
 
