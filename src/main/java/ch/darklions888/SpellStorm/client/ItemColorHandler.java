29ee7976ab54164331dadbb11de329f75bd79de1
DarkLions <darklions888@gmail.com>
2020-09-14 22:01:25 +0200

    Update ItemColorHandler.java

--- src/main/java/ch/darklions888/SpellStorm/client/ItemColorHandler.java
+++ src/main/java/ch/darklions888/SpellStorm/client/ItemColorHandler.java
@@ -1,5 +1,7 @@
 package ch.darklions888.SpellStorm.client;
 
+import ch.darklions888.SpellStorm.lib.MagicSource;
+import ch.darklions888.SpellStorm.objects.items.weapons.BaseSwordItem;
 import ch.darklions888.SpellStorm.registries.ItemInit;
 import net.minecraft.client.renderer.color.ItemColors;
 
@@ -8,16 +10,20 @@ public class ItemColorHandler {
 	public static ItemColors init(ItemColors colorsIn) {
 		
 		colorsIn.register((itemStack, color) -> {
-			return color == 0 ? 0x8A0000 : -1;
+			return color == 0 ? MagicSource.getMagicSourceColour(MagicSource.DARKMAGIC) : -1;
 		}, ItemInit.MAGICAL_INK_DARK.get());
 		
 		colorsIn.register((itemStack, color) -> {
-			return color == 0 ? 0xFFB600 : -1;
+			return color == 0 ? MagicSource.getMagicSourceColour(MagicSource.LIGHTMAGIC) : -1;
 		}, ItemInit.MAGICAL_INK_LIGHT.get());
 		
 		colorsIn.register((itemStack, color) -> {
-			return color == 0 ? 10 : -1;
+			return color == 0 ? MagicSource.getMagicSourceColour(MagicSource.UNKNOWNMAGIC) : -1;
 		}, ItemInit.MAGICAL_INK_UNKNOWN.get());
+		
+		colorsIn.register((itemStack, color) -> {
+			return color == 0 ? itemStack.getItem() instanceof BaseSwordItem ? MagicSource.getMagicSourceColour(((BaseSwordItem)itemStack.getItem()).getDefaultSource()) : 0xFFFFF : -1;
+		}, ItemInit.MANA_INFUSED_SWORD.get());
 		
 		return colorsIn;
 	}
