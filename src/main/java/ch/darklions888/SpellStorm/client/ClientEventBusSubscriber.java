5a81d35b64e187165a34765c70c9fcd78648994f
DarkLions <darklions888@gmail.com>
2020-10-06 11:42:59 +0200

    Update ClientEventBusSubscriber.java

--- src/main/java/ch/darklions888/SpellStorm/client/ClientEventBusSubscriber.java
+++ src/main/java/ch/darklions888/SpellStorm/client/ClientEventBusSubscriber.java
@@ -7,12 +7,15 @@ import ch.darklions888.SpellStorm.client.gui.SoulExtractorScreen;
 import ch.darklions888.SpellStorm.lib.Lib;
 import ch.darklions888.SpellStorm.registries.BlockInit;
 import ch.darklions888.SpellStorm.registries.ContainerTypesInit;
+import ch.darklions888.SpellStorm.registries.EntityInit;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -27,10 +30,19 @@ public class ClientEventBusSubscriber {
 		ContainerTypesInit.BOOK_OF_SPELLS.ifPresent(container -> ScreenManager.registerFactory(container, BookOfSpellsScreen::new));
 		ContainerTypesInit.MAGICAL_FORGE.ifPresent(container -> ScreenManager.registerFactory(container, MagicalForgeScreen::new));
 
+		registerRenderTypes();
+		registerEntityRenderers();
+		
+		ItemColorHandler.init(Minecraft.getInstance().getItemColors());
+	}
+	
+	private static void registerRenderTypes() {
 		RenderTypeLookup.setRenderLayer(BlockInit.MAGICAL_TREE_SAPLING.get(), RenderType.getCutout());
 		RenderTypeLookup.setRenderLayer(BlockInit.SOUL_EXTRACTOR.get(), RenderType.getTranslucent());
 		RenderTypeLookup.setRenderLayer(BlockInit.MAGICAL_FORGE.get(), RenderType.getTranslucent());
-		
-		ItemColorHandler.init(Minecraft.getInstance().getItemColors());
+	}
+	
+	private static void registerEntityRenderers() {
+		RenderingRegistry.registerEntityRenderingHandler(EntityInit.MAGICAL_FIREBALL.get(), renderManager -> new SpriteRenderer<>(renderManager, Minecraft.getInstance().getItemRenderer()));
 	}
 }
