b9b0076702b81223f2e4fe257e623962ba609124
DarkLions <darklions888@gmail.com>
2020-10-06 11:44:21 +0200

    Added MagicalFireballEntity

--- src/main/java/ch/darklions888/SpellStorm/registries/EntityInit.java
+++ src/main/java/ch/darklions888/SpellStorm/registries/EntityInit.java
@@ -1,17 +1,17 @@
 package ch.darklions888.SpellStorm.registries;
 
 import ch.darklions888.SpellStorm.lib.Lib;
+import ch.darklions888.SpellStorm.objects.entities.projectiles.MagicalFireballEntity;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class EntityInit {
 
 	public static final DeferredRegister<EntityType<?>> REGISTER_ENTITY = DeferredRegister.create(ForgeRegistries.ENTITIES, Lib.MOD_ID);
-	
-	/*
-	public static final RegistryObject<EntityType<DragonBallEntity>> DRAGON_BALL_ENTITY = REGISTER_ENTITY.register
-			("dragon_ball_entity", () -> EntityType.Builder.<DragonBallEntity>create(DragonBallEntity::new, EntityClassification.MISC)
-			.build(Lib.location("dragon_ball_entity").toString()));
-			*/
+
+	public static final RegistryObject<EntityType<MagicalFireballEntity>> MAGICAL_FIREBALL = REGISTER_ENTITY.register(Lib.RegistryNames.MAGICAL_FIREBALL_ENTITY_STR, () -> EntityType.Builder.<MagicalFireballEntity>create(MagicalFireballEntity::new, EntityClassification.MISC).size(0.3125F, 0.3125F).setTrackingRange(64).setUpdateInterval(1).setShouldReceiveVelocityUpdates(true).build(Lib.RegistryNames.MAGICAL_FIREBALL_ENTITY_RS.toString()));
+			
 }
