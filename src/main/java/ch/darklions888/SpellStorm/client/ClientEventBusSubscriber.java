f3a4f63c36d9e1446bf66b484887adb38489f3b3
DarkLions <darklions888@gmail.com>
2020-10-08 12:00:11 +0200

    Cleaned ClientRegistry's code

--- src/main/java/ch/darklions888/SpellStorm/client/ClientEventBusSubscriber.java
+++ src/main/java/ch/darklions888/SpellStorm/client/ClientEventBusSubscriber.java
@@ -4,10 +4,12 @@ import ch.darklions888.SpellStorm.client.gui.BookOfSpellsScreen;
 import ch.darklions888.SpellStorm.client.gui.MagicalForgeScreen;
 import ch.darklions888.SpellStorm.client.gui.ManaInfuserScreen;
 import ch.darklions888.SpellStorm.client.gui.SoulExtractorScreen;
+import ch.darklions888.SpellStorm.client.renderer.GateWayTileEntityRenderer;
 import ch.darklions888.SpellStorm.lib.Lib;
 import ch.darklions888.SpellStorm.registries.BlockInit;
 import ch.darklions888.SpellStorm.registries.ContainerTypesInit;
 import ch.darklions888.SpellStorm.registries.EntityInit;
+import ch.darklions888.SpellStorm.registries.TileEntityInit;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
@@ -15,6 +17,7 @@ import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -25,13 +28,10 @@ public class ClientEventBusSubscriber {
 	@SubscribeEvent
 	public static void clientSetup(FMLClientSetupEvent event) {
 
-		ContainerTypesInit.MANA_INFUSER.ifPresent(container -> ScreenManager.registerFactory(container, ManaInfuserScreen::new));
-		ContainerTypesInit.SOUL_EXTRACTOR.ifPresent(container -> ScreenManager.registerFactory(container, SoulExtractorScreen::new));
-		ContainerTypesInit.BOOK_OF_SPELLS.ifPresent(container -> ScreenManager.registerFactory(container, BookOfSpellsScreen::new));
-		ContainerTypesInit.MAGICAL_FORGE.ifPresent(container -> ScreenManager.registerFactory(container, MagicalForgeScreen::new));
-
+		registerContainerScreen();
 		registerRenderTypes();
 		registerEntityRenderers();
+		registerTileEntityRenderer();
 		
 		ItemColorHandler.init(Minecraft.getInstance().getItemColors());
 	}
@@ -44,5 +44,17 @@ public class ClientEventBusSubscriber {
 	
 	private static void registerEntityRenderers() {
 		RenderingRegistry.registerEntityRenderingHandler(EntityInit.MAGICAL_FIREBALL.get(), renderManager -> new SpriteRenderer<>(renderManager, Minecraft.getInstance().getItemRenderer()));
+	}
+	
+	private static void registerContainerScreen() {
+		ContainerTypesInit.MANA_INFUSER.ifPresent(container -> ScreenManager.registerFactory(container, ManaInfuserScreen::new));
+		ContainerTypesInit.SOUL_EXTRACTOR.ifPresent(container -> ScreenManager.registerFactory(container, SoulExtractorScreen::new));
+		ContainerTypesInit.BOOK_OF_SPELLS.ifPresent(container -> ScreenManager.registerFactory(container, BookOfSpellsScreen::new));
+		ContainerTypesInit.MAGICAL_FORGE.ifPresent(container -> ScreenManager.registerFactory(container, MagicalForgeScreen::new));
+
+	}
+	
+	private static void registerTileEntityRenderer() {
+		ClientRegistry.bindTileEntityRenderer(TileEntityInit.GATEWAY_TILEENTITY.get(), GateWayTileEntityRenderer::new);
 	}
 }
