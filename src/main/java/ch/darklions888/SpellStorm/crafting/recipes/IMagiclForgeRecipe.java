03d975e80e0f018cc8406b6cdf2d51a633041186
DarkLions <darklions888@gmail.com>
2020-10-08 12:01:19 +0200

    Delete IMagiclForgeRecipe.java

--- src/main/java/ch/darklions888/SpellStorm/crafting/recipes/IMagiclForgeRecipe.java
+++
@@ -1,8 +0,0 @@
-package ch.darklions888.SpellStorm.crafting.recipes;
-
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.crafting.IRecipe;
-
-public interface IMagiclForgeRecipe extends IRecipe<IInventory>{
-
-}
