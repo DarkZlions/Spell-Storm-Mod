b0a2371f3bd1c4a15ffdd787ed4c467cf55d6842
DarkLions <darklions888@gmail.com>
2020-10-10 02:32:46 +0200

    Create EndGatewayFragmentItem.java

---
+++ src/main/java/ch/darklions888/SpellStorm/objects/items/EndGatewayFragmentItem.java
@@ -0,0 +1,11 @@
+package ch.darklions888.SpellStorm.objects.items;
+
+import ch.darklions888.SpellStorm.lib.MagicSource;
+import ch.darklions888.SpellStorm.lib.ManaPower;
+
+public class EndGatewayFragmentItem extends BaseItem {
+
+	public EndGatewayFragmentItem(Properties properties) {
+		super(MagicSource.DARKMAGIC, ManaPower.VERYHIGH, null, false, properties);
+	}
+}
