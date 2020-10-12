80086248f4e5dab70955403b7fbc4d845cc931ff
DarkLions <darklions888@gmail.com>
2020-10-12 18:41:31 +0200

    Replaced playerEntity with a Entity

--- src/main/java/ch/darklions888/SpellStorm/util/helpers/mathhelpers/RayTraceHelper.java
+++ src/main/java/ch/darklions888/SpellStorm/util/helpers/mathhelpers/RayTraceHelper.java
@@ -1,6 +1,6 @@
 package ch.darklions888.SpellStorm.util.helpers.mathhelpers;
 
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
@@ -9,11 +9,11 @@ import net.minecraft.world.World;
 
 public class RayTraceHelper 
 {
-	   public static RayTraceResult CustomrayTrace(World worldIn, PlayerEntity player, RayTraceContext.FluidMode fluidMode, double range) 
+	   public static RayTraceResult CustomrayTrace(World worldIn, Entity entityIn, RayTraceContext.FluidMode fluidMode, double range) 
 	   {
-		      float f = player.rotationPitch;
-		      float f1 = player.rotationYaw;
-		      Vector3d vec3d = player.getEyePosition(1.0F);
+		      float f = entityIn.rotationPitch;
+		      float f1 = entityIn.rotationYaw;
+		      Vector3d vec3d = entityIn.getEyePosition(1.0F);
 		      float f2 = MathHelper.cos(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
 		      float f3 = MathHelper.sin(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
 		      float f4 = -MathHelper.cos(-f * ((float)Math.PI / 180F));
@@ -21,6 +21,6 @@ public class RayTraceHelper
 		      float f6 = f3 * f4;
 		      float f7 = f2 * f4;
 		      Vector3d vec3d1 = vec3d.add((double)f6 * range, (double)f5 * range, (double)f7 * range);
-		      return worldIn.rayTraceBlocks(new RayTraceContext(vec3d, vec3d1, RayTraceContext.BlockMode.OUTLINE, fluidMode, player));
+		      return worldIn.rayTraceBlocks(new RayTraceContext(vec3d, vec3d1, RayTraceContext.BlockMode.OUTLINE, fluidMode, entityIn));
 	   }
 }
